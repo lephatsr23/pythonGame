199fb0306168850eb4465d63da601bb11e648ced
PhatTruong <phatdat93@gmail.com>
2020-09-13 21:35:04 +0700

    level game, add words of varying lenght

--- nine_lives.py
+++ nine_lives.py
@@ -1,10 +1,30 @@
 import random
 lives = 9 
-words = ['fairy', 'teeth', 'shirt','otter','plane']
-secret_word = random.choice(words)
-clue = list('?????')
+words_Easy = ['fairy', 'teeth', 'shirt','otter','plane']
+words_Normal = ['cocacola', 'macdonal', 'circlek','lotteria','cinima']
+words_Hard = ['hipopotamus', 'homolocu', 'Gastonia','camila','dinosaur','neoastrong']
+
+difficulty = input('Choice difficulty (type 1, 2, or 3):\n 1) Easy\n 2) Normal\n 3) Hard\n')
+difficulty = int(difficulty)
+
+if difficulty == 1:
+	lives = 12
+	secret_word = random.choice(words_Easy)
+elif difficulty == 2:
+	lives = 9
+	secret_word = random.choice(words_Normal)
+else:
+	secret_word = random.choice(words_Hard)
+	lives = 6
+
+clue = list()
 heart_symbol = u'\u2665'
 guess_word_correctly = False
+
+index = 0
+while index < len(secret_word):
+	clue.append('?')
+	index = index + 1
 
 def update_clue(guess_letter , secret_word, clue):
 	index = 0
@@ -29,6 +49,7 @@ while lives > 0:
 	guess = input('Guess a letter or the whole word:')
 
 	if guess == 'quit':
+		guess_word_correctly = 2
 		break
 
 	if guess == secret_word:
@@ -45,7 +66,9 @@ while lives > 0:
 		guess_word_correctly = True
 		break
 
-if guess_word_correctly:
+if guess_word_correctly == True:
 	print('You won! The correct word was ' + secret_word)
+elif guess_word_correctly == 2:
+	print('You exit game')
 else:
 	print('You lose! The correct word was '+ secret_word)
